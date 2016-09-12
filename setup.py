a75c67edb8af0f738ca729869728a12714b55d26
Emlyn O'Regan <Emlyn O'Regan>
2016-09-12 18:15:27 +0930

    Fixed. This is the successful version that creates the 1.0.3b1 package

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
+import sUTL
 
 here = path.abspath(path.dirname(__file__))
 
@@ -22,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.1b1',
+    version='1.0.3b1',
 
     description='A sUTL implementation for python',
     long_description=long_description,
@@ -68,7 +69,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
